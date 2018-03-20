7437777d6189a9ccbdf85f83bda5cf7381907b7d
Nicolas Novello <nico.novello@gmail.com>
2018-03-20 16:53:20 +0100

    clear old code

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ import { homePageReducer } from './../src/modules/HomePage'
 import { shouldFetchHomePageData } from './../src/modules/HomePage/actions'
 import { aboutPageReducer } from './../src/modules/AboutPage'
 import { languageProviderReducer } from './../src/modules/LanguageProvider'
-import { changeLocale } from './../src/modules/LanguageProvider/actions'
 const PORT = process.env.PORT || 5000
 
 const store = createStore(combineReducers({
@@ -23,7 +22,6 @@ const store = createStore(combineReducers({
 
 store.dispatch(
   shouldFetchHomePageData()
-  // changeLocale()
 ).then(() => {
   // Server run from root of project
   express()
