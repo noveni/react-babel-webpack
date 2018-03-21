591b1b9ee453906b1c4f90786f45ad9d63790209
Nicolas Novello <nico.novello@gmail.com>
2018-03-21 12:28:53 +0100

    install pre-commit to check all js code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 
-import AppRoot from './AppRoot';
+import AppRoot from './AppRoot'
 
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: ["babel-polyfill",'./src/app.js'],
+  entry: ['babel-polyfill','./src/app.js'],
   plugins: [
     new CleanWebpackPlugin(['build'], {
       exclude: ['.keep']
@@ -14,7 +14,7 @@ module.exports = {
   devtool: 'eval-source-map',
   devServer: {
     contentBase: './',
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     hot: true,
     historyApiFallback: true
   },
